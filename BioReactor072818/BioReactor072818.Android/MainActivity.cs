9afb004591fcc2248b828ab76f54d5aeecfea839
snyderitis <tasnyder98@gmail.com>
2019-01-30 23:34:50 -0800

    Syncing

--- BioReactor072818/BioReactor072818.Android/MainActivity.cs
+++ BioReactor072818/BioReactor072818.Android/MainActivity.cs
@@ -8,7 +8,10 @@ using Android.Widget;
 using Android.OS;
 using Android;
 using Android.Support.V4.App;
+//using Android.Things.Pio;
 using System.Diagnostics;
+using Java.Util.Concurrent;
+using Java.Lang;
 
 namespace BioReactor072818.Droid
 {
@@ -17,6 +20,8 @@ namespace BioReactor072818.Droid
     {
 
         public const int EXTERNAL_STORAGE = 1;
+        //private IUartDevice Arduino;
+        //private PeripheralManager PM;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -24,16 +29,58 @@ namespace BioReactor072818.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+
+            //For the popup window to get text for the chemical and additives
             Rg.Plugins.Popup.Popup.Init(this, bundle);
+
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage }, EXTERNAL_STORAGE);
+
+            //Request permissions
+            ActivityCompat.RequestPermissions(this, new System.String[] { Manifest.Permission.WriteExternalStorage }, EXTERNAL_STORAGE);
             //Checking if able to write to public storage, if unable to, ask for permission
             if (!Android.OS.Environment.MediaMounted.Equals(Android.OS.Environment.ExternalStorageState))
             {
-                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage }, EXTERNAL_STORAGE);
+                ActivityCompat.RequestPermissions(this, new System.String[] { Manifest.Permission.WriteExternalStorage }, EXTERNAL_STORAGE);
             }
+
+
+            //var PM = PeripheralManager.Instance;
+            //var gpios = PM.GpioList;
+            //foreach (var gpio in gpios)
+            //{
+            //    System.Diagnostics.Debug.Print(gpio);
+            //}
+            //
             LoadApplication(new App());
         }
+
+
+
+        /// <summary>
+        /// 
+        /// 
+        /// LEFT OFF HERE, TRYING TO RUN SOME CODE TO TALK TO ARDUINO
+        /// 
+        /// https://medium.com/@bastermark3/connecting-raspberry-pi-3-with-android-things-to-arduino-51d202006379
+        /// 
+        /// 
+        /// 
+        /// </summary>
+        public void TxRxArduino()
+        {
+            /*Arduino = PM.OpenUartDevice("UART0");
+            Arduino.SetBaudrate(9600);
+
+            Executors.NewSingleThreadScheduledExecutor().ScheduleAtFixedRate(new Runnable()
+            {
+                public void run()
+                {
+                    //write data
+                }
+            },0,1,TimeUnit.SECONDS);*/
+        }
+
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
